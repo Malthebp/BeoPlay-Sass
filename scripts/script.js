72cbf8d31bef38c0027263acdd9631b64016371d
Malthebp <malthebpet@gmail.com>
2017-02-27 22:07:56 +0100

    Beoplay opgave

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+    $("#Slide").click(function(){
+        $("#modal-box").slideUp("slow");
+        $("#Show-Modal-Box").slideDown("slow");
+
+    });
+
+    $("#SlideUp").click(function(){
+        $("#modal-box").slideDown("slow");
+        $("#Show-Modal-Box").slideUp("slow");
+
+    });
